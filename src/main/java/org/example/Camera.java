854bbbcf8d1621ce6710fd3a1d72bd370dc6e209
sophie15975 <47412837+sophie15975@users.noreply.github.com>
2024-04-11 19:03:11 +0300

    Update Camera.java
    удален  ненужный коммент

--- src/main/java/org/example/Camera.java
+++ src/main/java/org/example/Camera.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public interface Camera {
-    //Реализовать интерфейс Camera (камера) c методами takePhoto и recordVideo.
+    
 
     int takePhoto(int parameter);
     void recordVideo();
